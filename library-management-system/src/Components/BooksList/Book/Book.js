8254366d355d54eb68939df4680f73e1aae82814
RISHABRAVI23 <81304421+RISHABRAVI23@users.noreply.github.com>
2021-10-28 11:08:45 +0530

    Added RickRoll links

--- library-management-system/src/Components/BooksList/Book/Book.js
+++ library-management-system/src/Components/BooksList/Book/Book.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Book.css";
+import { Link } from "react-router-dom";
 
 export default function Book(props) {
 	return (
@@ -8,11 +9,11 @@ export default function Book(props) {
 			<img src={props.book_image} alt="" className="img" />
 			<p className="margin-top">By - {props.book_author}</p>
 			<div className="buttons-div">
-				<a className="buttons" href="#">
-					IDK
-				</a>
-				<a className="buttons" href="#">
-					IDK
+				<Link className="buttons" to={`view/${props.book_id}`}>
+					View
+				</Link>
+				<a className="buttons" href="https://youtu.be/iik25wqIuFo">
+					Borrow
 				</a>
 			</div>
 		</div>
