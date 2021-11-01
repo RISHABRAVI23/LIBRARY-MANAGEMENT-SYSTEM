fc46ab719f1dea635f76d2470672324b15410bce
RISHABRAVI23 <81304421+RISHABRAVI23@users.noreply.github.com>
2021-11-01 19:18:48 +0530

    Created Virtual environment in python(django) backend

--- library-management-system/src/Components/BooksList/Book/Book.js
+++ library-management-system/src/Components/BooksList/Book/Book.js
@@ -9,7 +9,7 @@ export default function Book(props) {
 			<img src={props.book_image} alt="" className="img" />
 			<p className="margin-top">By - {props.book_author}</p>
 			<div className="buttons-div">
-				<Link className="buttons" href="#">
+				<Link className="buttons" to="#">
 					IDK
 				</Link>
 				<a className="buttons" href="https://youtu.be/iik25wqIuFo">
